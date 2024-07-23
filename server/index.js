3771166d106ad6d0457ad5e0e1af928f7f3c9d8a
cjftns0211 <cjftns0211_@naver.com>
2024-07-23 17:01:32 +0900

    백엔드 수정 미 엔드포인트 작성

--- server/index.js
+++ server/index.js
@@ -15,7 +15,16 @@ app.get('/', function (req, res) {
 app.get('/main', function (req, res) {
     res.sendfile(__dirname + "/public/main.html")
 });
-
+app.get('/chart', function(req, res)) {
+    res.json([
+        {
+        ranking: 1,
+        title: '데드풀과 울버린'
+        satisfy: 99,
+        ratio: 44.4
+        url: '/movie?cate=action&sf?code={ranking}'}
+    ])
+}
 // req: request [요청]
 // res: response [응답]
 
